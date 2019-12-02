a0fbed2f673c57255cf15dac6ce4a2bd6fd4dc15
IvanS <ivan443931@gmail.com>
2019-12-02 23:03:48 +0300

    API project - add controller protected by Authorize and Auth settings to Startup

---
+++ IdentityServer4Test/Controllers/IdentityController.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer4Test.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(User.Claims.Select(c => new {c.Type, c.Value}));
+        }
+    }
+}
