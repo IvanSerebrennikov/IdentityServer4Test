f49331039eec1edd794a6e443a28dd3cf76955ed
IvanS <ivan443931@gmail.com>
2019-12-02 23:31:04 +0300

    Add ability for External App Client to get JWT token from identity Server

--- ExternalAppClient/Controllers/HomeController.cs
+++ ExternalAppClient/Controllers/HomeController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ExternalAppClient.Models;
+using IdentityModel.Client;
 
 namespace ExternalAppClient.Controllers
 {
@@ -26,6 +28,34 @@ namespace ExternalAppClient.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> GetTokenTest()
+        {
+            var client = new HttpClient();
+            var identityServerUrl = "https://localhost:5001";
+            var disco = await client.GetDiscoveryDocumentAsync(identityServerUrl);
+            if (disco.IsError)
+            {
+                return Content(disco.Error);
+            }
+
+            // request token
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "externalApp1",
+                ClientSecret = "externalApp1secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                return Content(disco.Error);
+            }
+
+            return Content(tokenResponse.Json.ToString());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
